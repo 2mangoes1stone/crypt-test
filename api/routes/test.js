5924fc2504c5d6763e470b4898cb87c79b6cf16e
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-06-28 11:57:15 +1000

    parsing fetch data

--- api/routes/test.js
+++ api/routes/test.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const fetch = require('isomorphic-fetch')
+const fetch = require('node-fetch')
 
 const router = express.Router()
 
 router.get('/test', (req,res) => {
   fetch('https://btc-e.com/api/3/ticker/btc_usd')
-    .then((testData) => {
-      res.json(testData)
+    .then((apires) => apires.json())
+    .then((json) => {
+      res.json(json)
     })
     .catch((error) => {
       res.json({ error:error })
--- api/server.js
+++ api/server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const testRouter = require('./routes/test')
 
+
 const server = express()
 
 server.use(bodyParser.json())
 
 server.use(testRouter)
+
+
+
+
 
 const port = 7777
 server.listen(port, () => {
